1c3f1ec002623636f0a1de0f67c8a14603b9a733
Lunare-dot <matheusalizsouza@gmail.com>
2026-03-24 16:30:21 -0300

    Ex002 - Conclusão da atividade
    - Resolução dos últimos requisitos

--- ExtensãoWeb/Atividades/Ex002/script.js
+++ ExtensãoWeb/Atividades/Ex002/script.js
@@ -4,10 +4,14 @@ function main() {
     const inputText = document.getElementById('uEntry');
     const b1 =  document.getElementById('botao1');
     const b2 = document.getElementById('botao2');
+    const b3 = document.getElementById('botao3');
+    const b4 =  document.getElementById('resetB')
     
     minMaxButton(b1, b2);
     inputList(inputText);
     stringCount(inputText);
+    addItems(b3);
+    resetButton(b4);
 }
 
 function minMaxButton(b1, b2) {
@@ -63,4 +67,33 @@ function stringCount(input) {
     input.addEventListener("input", function() {
         count.textContent = input.value.replace(/\s/g, '').length;
     });
+}
+
+function addItems(b3) {
+    const options = document.getElementById('opts');
+    const messageArea3 = document.getElementById('m3');
+
+    messageArea3.style.color = "black";
+
+    b3.onclick = function(event) {
+        event.preventDefault();
+
+        const list = options.value == "opt1" 
+            ? document.createElement('ul') 
+            : document.createElement('ol');
+
+        ["Item 1", "Item 2", "Item 3"].forEach(text => {
+            const li = document.createElement('li');
+            li.textContent = text;
+            list.appendChild(li);
+        });
+
+        messageArea3.appendChild(list);
+    }
+}
+
+function resetButton(b4) {
+    b4.onclick = function() {
+        location.reload();
+    }
 }
