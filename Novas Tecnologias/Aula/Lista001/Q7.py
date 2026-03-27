3cd96c27416a9c37d75654eb70d91ad7fb6441a7
Lunare-dot <matheusalizsouza@gmail.com>
2026-03-27 09:39:20 -0300

    Apagando arquivos fora de lugar

--- Novas Tecnologias/Aula/Lista001/Q7.py
+++
@@ -1,7 +0,0 @@
-def vowelCount(sentence):
-    vowels = "aeiou"
-    return sum(1 for char in sentence.lower() if char in vowels)
-
-sentence = input("Insira uma frase: ")
-contador = vowelCount(sentence)
-print(f"Existem {contador} vogais nessa frase.")
