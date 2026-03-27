a2d68e0018552d6b425094e56b3e907a99e27927
Matheus <matheusalizsouza@gmail.com>
2026-03-27 09:45:07 -0300

    Recuperando Novas Tecnologias

---
+++ Novas Tecnologias/Aula/Lista003/Q1.py
@@ -0,0 +1,21 @@
+def listOrd(inter):
+    inter.sort(key=lambda x: x[0])
+    list = []
+    
+    cStart, cEnd = inter[0]
+    for start, end in inter[1:]:
+        if start <= cEnd:
+            cEnd = max(cEnd, end)
+        else:
+            list.append((cStart, cEnd))
+            cStart, cEnd = start, end
+        
+    list.append((cStart, cEnd))
+    return list
+    
+def main():
+    uInter = [(1, 4), (2, 5), (7,9)]
+    print(listOrd(uInter))
+    
+if __name__ == "__main__":
+    main()
