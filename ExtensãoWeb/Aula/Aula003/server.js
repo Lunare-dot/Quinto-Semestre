9bc59659771badec272fd56262e35c3a710c87e7
Lunare-dot <matheusalizsouza@gmail.com>
2026-05-08 09:50:32 -0300

    feat: add @ljharb/tsconfig dependency and configure project structure

--- ExtensãoWeb/Aula/Aula003/server.js
+++ ExtensãoWeb/Aula/Aula003/server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//rotas -- rota padrão (/)
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/sobre', (req, res) => {
+  res.send('Página Sobre!')
 })
 
 app.listen(port, () => {
