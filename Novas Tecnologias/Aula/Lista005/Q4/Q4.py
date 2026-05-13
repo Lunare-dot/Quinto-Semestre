d38e4d9b3b0935b6b67ca940aae968dcb70a383c
Lunare-dot <matheusalizsouza@gmail.com>
2026-05-13 20:52:21 -0300

    fix: correct variable name from 'trigrinho' to 'tigrinho' in Q4.py

--- Novas Tecnologias/Aula/Lista005/Q4/Q4.py
+++ Novas Tecnologias/Aula/Lista005/Q4/Q4.py
@@ -1,8 +1,8 @@
 from jogo.dados import diceRoll
 from jogo.eventos import eventSort
 
-trigrinho = diceRoll()
-resultado = eventSort(trigrinho)
+tigrinho = diceRoll()
+resultado = eventSort(tigrinho)
 
-print(f"Face do dado: {trigrinho}")
+print(f"Face do dado: {tigrinho}")
 print(f"Divinação: {resultado}")
